5635a095422e3acd5213389c62c0cb8e98afaab7
jrvldam <jrvldam@gmail.com>
2023-03-20 00:30:58 +0100

    resistor-color

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,23 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
+type Color = keyof typeof colors
+
+const colors = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+} as const
+
+export const colorCode = (color: Color) => {
+  return colors[color]
 }
 
-export const COLORS = undefined
+export const COLORS = Object.keys(colors)
+
+
+
